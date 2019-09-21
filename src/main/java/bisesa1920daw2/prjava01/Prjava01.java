2458efc3b7f114f330def3f965e5b8d11228f98b
bisesa1920daw2@gmail.com <>
2019-09-21 12:48:35 +0200

    Versió 0.1 del projecte prjava01

---
+++ src/main/java/bisesa1920daw2/prjava01/Prjava01.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ */
+package bisesa1920daw2.prjava01;
+
+import java.io.*;
+
+/**
+ *
+ * @author bisesa1920daw2
+ */
+public class Prjava01 {
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>"); bw.newLine();
+            bw.write("  <head>"); bw.newLine();
+            bw.write("      <title>"); bw.newLine();
+            bw.write("          Nova p&agrave;gina web"); bw.newLine();
+            bw.write("      </title>"); bw.newLine();
+            bw.write("  </head>"); bw.newLine();
+            bw.write("  <body>"); bw.newLine();
+            bw.write("      Nova p&agrave;gina web"); bw.newLine();
+            bw.write("  </body>"); bw.newLine();
+            bw.write("</html>"); bw.newLine();
+            bw.close();
+        }
+    }
+}
